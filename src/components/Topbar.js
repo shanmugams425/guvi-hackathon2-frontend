92ef660ce4e781bb3eada6f42e44ae0cc625b627
shanmugam R <shanmugams425@gmail.com>
2022-07-11 12:39:13 +0530

    s

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,4 +1,6 @@
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Link } from "react-router-dom";
+
 
 function Topbar() {
   
@@ -9,19 +11,18 @@ function Topbar() {
           <Navbar.Brand  className='h3'>LensRental</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
+          <Nav className="me-auto">
               <Nav.Link href='home'>Home</Nav.Link>
               <Nav.Link href="product">Products</Nav.Link>
               <Nav.Link href="contact">Contact</Nav.Link>
             </Nav>
-           
           </Navbar.Collapse>
           </Container>
         </Navbar>
     
        </div>
         )
-}
+} 
   
 
 
